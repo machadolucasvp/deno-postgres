86e4a261154b8f821e07eb2802058f16944b29d1
Lucas Machado <lucasvufma@gmail.com>
2020-05-18 20:37:45 -0300

    feat: add builder pattern for book model

--- model/book.ts
+++ model/book.ts
@@ -1,5 +1,42 @@
-export default interface Book {
-    id: number;
-    title: string;
-    isbn: string;
+export default interface IBook {
+  id?: number;
+  title?: string;
+  isbn?: string;
+}
+
+export class Book implements IBook {
+  id?: number;
+  title?: string;
+  isbn?: string;
+
+  constructor(book: Book) {
+    Object.assign(this, book);
+  }
+  
+  static builder(): BookBuilder {
+    return new BookBuilder();
+  }
+}
+
+class BookBuilder {
+  id?: number;
+  title?: string;
+  isbn?: string;
+
+  build(): Book {
+    return new Book(this);
+  }
+
+  withId(id: number): BookBuilder {
+    this.id = id;
+    return this;
+  }
+  withTitle(title: string): BookBuilder {
+    this.title = title;
+    return this;
+  }
+  withIsbn(isbn: string): BookBuilder {
+    this.isbn = isbn;
+    return this;
+  }
 }
